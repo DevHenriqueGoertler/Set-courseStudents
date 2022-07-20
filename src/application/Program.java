2764f18b28851faa2cc7df4390a965cd9d8f0d46
Henrique Goertler <dev.henriquegoertler@gmail.com>
2022-07-20 00:50:56 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Set<Integer> A = new HashSet<>();
+		Set<Integer> B = new HashSet<>();
+		Set<Integer> C = new HashSet<>();
+		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i=0; i < n; i++) {
+			A.add(sc.nextInt());
+		}
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for (int i=0; i < n; i++) {
+			B.add(sc.nextInt());
+		}
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for (int i=0; i < n; i++) {
+			C.add(sc.nextInt());
+		}
+		
+		Set<Integer> totalStudents = new HashSet<>(A);
+		totalStudents.addAll(B);
+		totalStudents.addAll(C);
+		
+		System.out.println("Total Students: " + totalStudents.size());
+		
+		sc.close();
+	}
+}
